60a0353a6bc3a69b49a5f05c92445ea157e3987b
Sakunaph41 <50283125+Sakunaph41@users.noreply.github.com>
2020-03-11 19:50:08 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'BvDsy8t/wkYCho4aIR/uK1JJquwnnTI7vnenWiu3Df6TEvLoBptYR7HonAtmHUKDQOffAkWetc4iw4Mkw7u4rlMM/VsBB6A5D7DfZ3NjO9kwWKC6wiHm3vvk86eBVCKUyT1nwxoYwLl0ok5n1JxrhQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'Ql70DWg9FQBL9Olv90EWoAHHKVVNaSyjSPEfQuELl6U6dA6q8P0rquXoiEcvn5TavrUY2gL2nJ4tLlq+JpdCYEox7QPIBmf97vSb8ZP3fE4d/4OL0skJVSS1XzvpvlyhckvCmKV2pNHUkLIHH1K7UQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
